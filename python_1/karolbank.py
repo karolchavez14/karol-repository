ee29c15f752524a8cbf331f9b743e15917560a42
SA101 <sa39@Skills-Academy-101.local>
2024-11-20 16:31:52 -0500

    python bank

--- python_1/karolbank.py
+++ python_1/karolbank.py
@@ -7,4 +7,16 @@ bank_balance = 0.03 #Balance in dollars.
 #Display the information
 def display_info():
     print("User Information:")
-    print()
+    print(f"Name: {name}")
+    print(f"Nickname: {nickname}")
+    print(f"age: {age}")
+    print(f"Bank Account Balance: ${bank_balance:.2f}")
+
+if bank_balance < 20:
+    print("You are poor!!")
+
+elif bank_balance > 10:
+    print("you are not pobre, you ate")
+
+#this is the actualitation of the program :)
+display_info()
