4a2ba2bc03af2d26be5f23117662456452414004
SA101 <sa39@Skills-Academy-101.local>
2024-11-25 18:16:52 -0500

    python_website

---
+++ python_1/karol_website.py
@@ -0,0 +1,12 @@
+import sys
+
+# Simple database for chemical elements
+periodic_table = {
+    "H": {"name": "Hydrogen", "atomic_number": 1, "atomic_mass": 1.008},
+    "He": {"name": "Helium", "atomic_number": 2, "atomic_mass": 4.00},
+    "O": {"name": "Oxygen", "atomic_number": 8, "atomic_mass": 16.00},
+    "C": {"name": "Carbon", "atomic_number": 6, "atomic_mass": 12.01},
+    "N": {"name": "Nitrogen", "atomic_number": 7, "atomic_mass": 14.01},
+    "Na": {"name": "Sodium", "atomic_number": 11, "atomic_mass": 22.99},
+    "Cl": {"name": "Chlorine", "atomic_number": 17, "atomic_mass": 35.45},
+}
