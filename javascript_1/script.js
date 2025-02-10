64f879e449924390a67ad103f8a364fdca6dd6b9
SA101 <sa39@Skills-Academy-101.local>
2025-02-10 18:09:35 -0500

    javaproyect

---
+++ javascript_1/script.js
@@ -0,0 +1,12 @@
+// alert("hii"):
+
+
+let age = 16;
+if (age >= 18){
+    console.log('You are a adult');
+}
+else{
+    console.log('Stay in school!')
+}
+const birthYear = 2008;
+console.log(" My age is", age)
