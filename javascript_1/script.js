00d921e2545ebba62a36b83ee40986f7a81e93e2
SA101 <sa39@Skills-Academy-101.local>
2025-02-12 18:13:09 -0500

    java

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,12 +1,15 @@
-// alert("hii"):
+// alert("hii");
 
 
 let age = 16;
-if (age >= 18){
+const birthYear = 2008;
+if (age >= 18 && birthYear < 2010){
     console.log('You are a adult');
 }
 else{
-    console.log('Stay in school!')
+    console.log('you are a child!')
 }
-const birthYear = 2008;
-console.log(" My age is", age)
+console.log("My age is", age)
+
+let Myheading = document.getElementById("My heading");
+Myheading.textContent = " I have put text on the page with JavaScript";
