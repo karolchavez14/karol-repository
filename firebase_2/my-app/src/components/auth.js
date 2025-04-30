b7586c488c80544b2213f6ab8d98450c0af8863d
SA101 <sa39@Skills-Academy-101.local>
2025-04-30 17:34:00 -0400

    firebase

---
+++ firebase_2/my-app/src/components/auth.js
@@ -0,0 +1,57 @@
+import {auth} from "../config/firebase";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from "firebase/auth";
+import React, {useState} from "react";
+function Auth() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    console.log(auth?.currentUser?.email)
+
+async function signUp() {
+    try { 
+        await createUserWithEmailAndPassword(auth, email, password)
+    }
+    catch(err) {
+        alert(err)
+
+    }
+};
+async function signIn() {
+    try {
+        await signInWithEmailAndPassword(auth, email, password)
+    }
+    catch(err) {
+        alert(err)
+    }
+}
+async function logout() {
+    try{
+        await signOut(auth);
+    }catch(err) {
+        console.error(err);
+    }
+    }
+
+
+
+
+    return(
+        <div>
+            <input placeholder="Email.."
+                onChange={(e)   => setEmail (e.target.value)}
+                />
+                
+            <input placeholder="password.."
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button onClick={signUp}>Sign Up</button>
+            <button onClick={signIn}>Sign In</button>
+            <button onClick={signOut}>Sign Out</button>
+        </div>
+    )
+}
+
+export default Auth;
+
