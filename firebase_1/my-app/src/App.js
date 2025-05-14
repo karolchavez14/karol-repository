6a99aa6eb26a60f2adfa82c4e0d3f0464908d8f3
SA101 <sa39@Skills-Academy-101.local>
2025-05-14 17:44:32 -0400

    project_finished

--- firebase_1/my-app/src/App.js
+++ firebase_1/my-app/src/App.js
@@ -1,45 +1,15 @@
 import './App.css';
 import Auth from './components/auth';
 import Nav from './components/nav';
-import { db } from "./config/firebase";
-import {getDocs, collection} from 'firebase/firestore';
-import {useState, useEffect} from 'react'
+import Post from './components/post';
 
 function App() {
-  const [postsList, setPostsList] = useState([]);
-
-  const postsCollectionRef = collection(db, "Posts") //saving the variable that indicates which collection we want acsess
-  
-  useEffect(() => { // you cant make a use effect async, and we needed async in order to make it work
-      async function getPosts()  {
-      // reading the data
-      // Set the posts
-      try { 
-        const data = await getDocs(postsCollectionRef) // gets all the collections from firestore
-        const filteredData = data.docs.map((doc)=>({...doc.data(),id: doc.id}));
-        setPostsList(filteredData);
-      } catch(err) {
-        alert(err)
-        }
-      }
-      getPosts();
-    }, []);
 
 return (
     <div className="App">
       <Nav></Nav>
       <Auth></Auth>
-    <div>
-    {postsList.map((posts)=> (
-      <div>
-          <h1> Title: {posts.Title}</h1>
-          <p> Date: {posts.Date?.toDate().toLocaleString()}</p>
-          <p>By: {posts.Author}</p>
-          <p> Location: {posts.location}</p>
-          <img src={posts.ImageURL}></img>
-          </div>
-      ))}
-    </div>
+      <Post></Post>
     </div> 
   );
 }
